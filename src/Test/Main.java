5b11f2fb3fbe365c07471356673a8bb679ee27cd
Catalin_Buzgau <catalin_buzgau@epam.com>
2022-07-05 16:25:21 +0300

    food1

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,22 +1,7 @@
 package Test;
 
-import Showroom.Car;
-import Showroom.Vehicle;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the shop!");
-
-        Vehicle vehicle = new Vehicle(50_000,"Mercedes");
-        vehicle.startEngine();
-
-        Car car = new Car(5_300,"Audi","purple");
-        car.showColor();
-        car.payTax();
-
-        Person person = new Person(20,"Catalin","romanian",1.87);
-        person.checkAge();
-
         Food food = new Food("Apple",5,10.2);
         System.out.println(food.totalPrice());
         food.checkFood();
