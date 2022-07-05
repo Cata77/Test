9022eed1e1ce098faa5ad58eb3fc72e4e2300d6c
Catalin_Buzgau <catalin_buzgau@epam.com>
2022-07-05 17:39:08 +0300

    Clone rep change

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -5,7 +5,7 @@ import Showroom.Vehicle;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the shop!");
+        System.out.println("Welcome to the shop!!!");
 
         Vehicle vehicle = new Vehicle(50_000,"Mercedes");
         vehicle.startEngine();
